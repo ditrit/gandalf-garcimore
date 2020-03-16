dbe807ff908c76d25c6a8ef468b16cc49c5be521
Dev-Ubuntu <romain.fairant@orness.com>
2020-03-16 14:39:50 +0100

    prepare gandalf

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -3,7 +3,6 @@ package cluster
 import (
 	"fmt"
 	"garcimore/database"
-	"shoset/msg"
 	"shoset/net"
 	"time"
 )
@@ -20,6 +19,8 @@ func NewClusterMember(logicalName string) *ClusterMember {
 	member := new(ClusterMember)
 	member.chaussette = net.NewShoset(logicalName, "cl")
 	member.chaussette.Handle["cfgjoin"] = HandleConfigJoin
+	member.chaussette.Handle["cmd"] = HandleCommand
+	member.chaussette.Handle["event"] = HandleEvent
 
 	return member
 }
@@ -89,37 +90,4 @@ func CreateStore(bros []string) *[]string {
 	}
 
 	return &store
-}
-
-// HandleConfigJoin :
-func HandleConfigJoin(c *net.ShosetConn, message msg.Message) error {
-	cfg := message.(msg.ConfigJoin)
-	ch := c.GetCh()
-	dir := c.GetDir()
-	thisOne := ch.GetBindAddr()
-	newMember := cfg.GetBindAddress() // recupere l'adresse distante
-
-	switch cfg.GetCommandName() {
-	case "join":
-		//fmt.Printf("%s : event 'join' received from %s\n", thisOne, newMember)
-		if dir == "in" {
-			ch.Join(newMember)
-		}
-		cfgNewMember := msg.NewCfgMember(newMember)
-		ch.ConnsJoin.Iterate(
-			func(key string, val *net.ShosetConn) {
-				if key != newMember && key != thisOne {
-					val.SendMessage(cfgNewMember)
-					// fmt.Printf("%s : send event new 'member' %s to %s\n", thisOne, newMember, key)
-				}
-			},
-		)
-		if dir == "out" {
-		}
-
-	case "member":
-		//fmt.Printf("%s : event 'member' received from %s\n", thisOne, newMember)
-		ch.Join(newMember)
-	}
-	return nil
 }
--- connector/connector.go
+++ connector/connector.go
@@ -2,7 +2,6 @@ package connector
 
 import (
 	"fmt"
-	"shoset/msg"
 	"shoset/net"
 	"time"
 )
@@ -17,6 +16,8 @@ func NewConnectorMember(logicalName string) *ConnectorMember {
 	member := new(ConnectorMember)
 	member.chaussette = net.NewShoset(logicalName, "ag")
 	member.chaussette.Handle["cfgjoin"] = HandleConfigJoin
+	member.chaussette.Handle["cmd"] = HandleCommand
+	member.chaussette.Handle["event"] = HandleEvent
 
 	return member
 }
@@ -69,37 +70,4 @@ func ConnectorMemberJoin(logicalName, bindAddress, joinAddress string) (connecto
 	fmt.Printf("%s.JoinBrothers Join(%#v)\n", bindAddress, getBrothers(bindAddress, member))
 
 	return member
-}
-
-// HandleConfigJoin :
-func HandleConfigJoin(c *net.ShosetConn, message msg.Message) error {
-	cfg := message.(msg.ConfigJoin)
-	ch := c.GetCh()
-	dir := c.GetDir()
-	thisOne := ch.GetBindAddr()
-	newMember := cfg.GetBindAddress() // recupere l'adresse distante
-
-	switch cfg.GetCommandName() {
-	case "join":
-		//fmt.Printf("%s : event 'join' received from %s\n", thisOne, newMember)
-		if dir == "in" {
-			ch.Join(newMember)
-		}
-		cfgNewMember := msg.NewCfgMember(newMember)
-		ch.ConnsJoin.Iterate(
-			func(key string, val *net.ShosetConn) {
-				if key != newMember && key != thisOne {
-					val.SendMessage(cfgNewMember)
-					// fmt.Printf("%s : send event new 'member' %s to %s\n", thisOne, newMember, key)
-				}
-			},
-		)
-		if dir == "out" {
-		}
-
-	case "member":
-		//fmt.Printf("%s : event 'member' received from %s\n", thisOne, newMember)
-		ch.Join(newMember)
-	}
-	return nil
 }
